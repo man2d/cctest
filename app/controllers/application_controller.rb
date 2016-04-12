9ffc2db9e39950414ad6015ea33e86875855ea83
apryalkin <andrey.pryalkin@dellin.ru>
2016-04-12 12:57:19 +0300

    commiting bad code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  
+class Customer
+   @@no_of_customers=0
+   def initialize(id, name, addr)
+      @cust_id=id
+      @cust_name=name
+      @cust_addr=addr
+   end
+end
+
+
+  puts 1
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def something(a,b,c,d,e,f,g,h)
+   @badCodeSmells = 1
+   #We automatically generated one or more configuration files for this repo. You can download this configuration to customize it
+
+   a = [
+[1,2,3]
+]
+  end
 end
