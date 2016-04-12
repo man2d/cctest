eb43e12d4996db5399b2b5d9a7f9f6b613e06bef
apryalkin <andrey.pryalkin@dellin.ru>
2016-04-12 13:13:56 +0300

    fix

--- lib/new_class.rb
+++ lib/new_class.rb
@@ -1,9 +1,13 @@
 class Customer
    @@no_of_customers=0
    def initialize(id, name, addr)
-      @cust_id=id
-      @cust_name=name
+      $superlongvariablename = NewClass.new(id, name, name, name, name, addr)
+      @cust_id=id && return
+      @cust_name=name && return
       @cust_addr=addr
+      if a == 1 || b == c && asdfasfd = 123123 || asdfsadfadsf = 1
+        
+      end
    end
 end
 
